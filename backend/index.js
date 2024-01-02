19c1106c3003c8cfb96ed0fe51a29f1d443f4f61
Mark Ruzicka <marker11778@gmail.com>
2024-01-02 11:27:28 -0600

    counter working

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,14 @@ const io = new Server(server, {
 
 io.on("connection" , (socket) => {
     console.log(`User Connected: ${socket.id}`)
+
+    socket.on("add", (count) => {
+        socket.broadcast.emit('update', count)
+    })
+
+    socket.on("sub", (count) => {
+        socket.broadcast.emit('update', count)
+    })
 })
 
 server.listen(3001, () =>{
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,39 @@
 
 import './App.css';
 import io from 'socket.io-client'
+import { useEffect, useState } from 'react';
 
 const socket = io.connect("http://localhost:3001")
 
-const counter= 0 
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   const addOne = () => {
-    //socket.emit()
+    setCounter(counter + 1); 
+    socket.emit("add", counter + 1)
   };
+
+  const subOne = () => {
+    setCounter(counter - 1); 
+    socket.emit("sub", counter - 1)
+  };
+
+  useEffect(() => {
+    socket.on("update", (count) => {
+      console.log(typeof(count))
+      if (count !== undefined) {
+        setCounter(count);
+      } else {
+        console.error('Received invalid count object:', count);
+      }
+    });
+  }, [socket]);
 
   return (
     <div>
-      <button> - </button>
+      <button onClick={subOne}> - </button>
       <h1> {counter} </h1>
       <button onClick={addOne}> + </button>
     </div>
