08bc8ea44758fd247a12e01c0afb9ef1b9dbf44c
Mark Ruzicka <marker11778@gmail.com>
2024-01-02 11:03:46 -0600

    working interactiong between front and back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 
 import './App.css';
+import io from 'socket.io-client'
+
+const socket = io.connect("http://localhost:3001")
+
+const counter= 0 
 
 function App() {
+  const addOne = () => {
+    //socket.emit()
+  };
+
   return (
-    <div className="App">
+    <div>
+      <button> - </button>
+      <h1> {counter} </h1>
+      <button onClick={addOne}> + </button>
     </div>
   );
 }
